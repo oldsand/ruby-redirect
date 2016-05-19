f711875f4df7a64f061739a6481b2ff94b5dfed5
Oldsand <oldsand0401@gmail.com>
2016-05-19 20:09:45 +0800

    add redirect (have bug)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :links
 
   get 'links/about'
+
+  get 'os' => 'links#redirect'
+
+  resources :links
 
   root 'links#index'
   # The priority is based upon order of creation: first created -> highest priority.
